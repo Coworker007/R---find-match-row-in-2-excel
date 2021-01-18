74f2cdee6a1bd421d30ae27e4fc1c744cd8e4d51
Denis <66379817+Vladimirovich-denis@users.noreply.github.com>
2021-01-18 05:36:12 +0300

    complete

--- matching-datasets.R
+++ matching-datasets.R
@@ -1,8 +1,8 @@
 library(dplyr)
 
 # upload the two datasets under separate variables
-data_A11y <- read.csv("~/Fiverr/R/wadji/A11y.csv")
-data_non_A11y <- read.csv("~/Fiverr/R/wadji/Non_A11y.csv")
+data_A11y <- read.csv("A11y.csv")
+data_non_A11y <- read.csv("Non_A11y.csv")
 
 # check for duplicates in both datasets
 data_A11y[duplicated(data_A11y), ]
@@ -12,16 +12,26 @@ data_non_A11y[duplicated(data_non_A11y), ]
 data_A11y <- data_A11y[!duplicated(data_A11y), ]
 data_non_A11y <- data_non_A11y[!duplicated(data_non_A11y),]
 
-for (value in unique(data_non_A11y$Bug.ID)){
-  clean_data_A11y <- data_A11y %>%
-    mutate(Match = ifelse(grepl(value, Bug.ID, fixed = TRUE), "Yes", "No"))
-  
+data_non_A11y_unique <- unique(data_non_A11y$Bug.ID)
+clean_data_A11y <- data_A11y
+clean_data_A11y$Match <- ""
+for (i in 1:nrow(clean_data_A11y)){
+  if(clean_data_A11y[i, "Bug.ID"] %in% data_non_A11y_unique){
+    clean_data_A11y[i, "Match"] <- "Yes"
+  }else{
+    clean_data_A11y[i, "Match"] <- "No"
+  }
 }
 
-for (value in unique(data_non_A11y$Bug.ID)){
-  clean_data_non_A11y <- data_non_A11y %>%
-    mutate(Match = ifelse(grepl(value, Bug.ID, fixed = TRUE), "Yes", "No"))
-  
+data_A11y_unique <- unique(data_A11y$Bug.ID)
+clean_data_non_A11y <- data_non_A11y
+clean_data_non_A11y$Match <- ""
+for(i in 1:nrow(clean_data_non_A11y)){
+  if(clean_data_non_A11y[i, "Bug.ID"] %in% data_A11y_unique){
+    clean_data_non_A11y[i, "Match"] <- "Yes"
+  }else{
+    clean_data_non_A11y[i, "Match"] <- "No"
+  }
 }
 
 View(clean_data_A11y)
@@ -29,3 +39,4 @@ View(clean_data_non_A11y)
 
 # create a dataframe with the yes columns
 positive_matches <- rbind(filter(clean_data_A11y, Match == "Yes"), filter(clean_data_non_A11y, Match == "Yes"))
+positive_matches <- positive_matches %>% unique()
